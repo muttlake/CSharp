aaeef22f70e5ede66df0dd35b6ddc0ff9239357f
Timothy Shepard <shepbop@gmail.com>
2018-05-22 09:11:31 -0400

    Added Session5

---
+++ Session5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Session5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Person
+    {
+        public override void SetName(string value)
+        {
+            if (string.isNullOrWhiteSpace(value))
+                throw new ArgumentNullException("value");
+        }
+    }
+}
