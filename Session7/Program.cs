13bdb117d299173b03bb1512434dba6dd92844e9
Timothy Shepard <shepbop@gmail.com>
2018-05-22 15:18:47 -0400

    Working on Session7

--- Session7/Program.cs
+++ Session7/Program.cs
@@ -7,6 +7,22 @@ namespace Session7
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            var dir = System.IO.Directory.GetCurrentDirectory();
+            var file = System.IO.Path.Combine(dir, "File.txt");
+            var content = "how now brown cow?";
+
+            //write
+            System.IO.File.WriteAllText(file, content);
+
+            //read
+            var read = System.IO.File.ReadAllText(file);
+            Trace.Assert(read.Equals(content));
+
+
         }
+
+
     }
 }
